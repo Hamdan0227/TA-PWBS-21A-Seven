dfc0a1cf35e05052e0cbdf6aad0b76ad81eef586
muhammda_hamdan_sobirin@teknokrat.ac.id <alfa.d.fin@gmail.com>
2023-11-26 22:25:42 +0700

    Dhafin: membuat fungsi detail

--- backend/app/Http/Controllers/Admin.php
+++ backend/app/Http/Controllers/Admin.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Madmin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class Admin extends Controller
 {
-    //
+    //buat fungsi detail data
+    function detailController($id)
+    {
+                 //isi nilai dari variabel "result" dari fungsi "detailData" dari model "Madmin" sesuai dengan isi parameter "id"
+                 $result = $this->model->detailData($id);
+
+                 //Kembalikan nilai variabel "result" ke dalam object "Admin"
+                return response(["Admin"=> $result], http_response_code());
+        
+    }
 }
--- backend/app/Models/Madmin.php
+++ backend/app/Models/Madmin.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Madmin extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    function detailData($id)
+    {
+        //tampilkan data dari "tb_data"
+        $query = DB::table('tb_data')
+                ->select("id","nama","golongan_darah","usia","telpon","create_at")
+                //->where(DB::raw("TO_BASE64(MD5(id))"),"$id")
+
+                ->get();
+        //mengirim hasil variabel "query" ke controller "admin"
+        return $query;
+    }
+
 }
