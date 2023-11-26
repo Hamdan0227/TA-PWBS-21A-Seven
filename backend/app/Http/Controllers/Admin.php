59733d5b92feba062b76456351975fd89bf71afa
muhammad_hamdan_sobirin@teknokrat.ac.id <Priaagung12@gmail.com>
2023-11-26 22:45:47 +0700

    Agung:Membuat function searchControler

--- backend/app/Http/Controllers/Admin.php
+++ backend/app/Http/Controllers/Admin.php
@@ -8,12 +8,12 @@ class Admin extends Controller
 {
      //buuat fungsi untuk pencarian data
     function searchController($keyword)
-    {
+    { 
           //isi nilai variabel "relsult dari fungsi "searchData"dari model "Madmin" sesuai dengan isi parameter "keyword"
        $result = $this->model->searchData
        ($keyword);
 
-       //kembalikan nilai variabel "result" kedalam object "mahasiswa"
+       //kembalikan nilai variabel "result" kedalam object "admin"
        return response(["Admin" =>$result],http_response_code());
     }
 }
