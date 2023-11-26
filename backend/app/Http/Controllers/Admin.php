80cba30f358701ab3c90c091673bc1780bb7eb20
Hamdan0227 <muhammad_hamdan_sobirin@teknokrat.ac.id>
2023-11-26 22:22:21 +0700

    master: Membuat functin get controller dan model getData

--- backend/app/Http/Controllers/Admin.php
+++ backend/app/Http/Controllers/Admin.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Madmin;
+use Illuminate\Support\Facedes\Mail;
 
 class Admin extends Controller
 {
-    //
+    //buat variabel
+    protected $model;
+
+    //buat fungsi global
+    function __construct()
+    {
+        //inisialisasi variabel "model" dari model "Madmin"
+        $this->model = new Madmin();
+    }
+
+    function getController()
+    {
+        //isi nilai dari variabel "result" dari fungsi "getData" dari model "Madmin"
+        $result = $this->model->getData();
+
+        //Kembalikan nilai variabel "result" ke dalam object "admin"
+        return response(["Admin"=> $result], http_response_code());
+    }
 }
--- backend/app/Models/Madmin.php
+++ backend/app/Models/Madmin.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Madmin extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+
+    //buat fungsi untuk ambil data "tb_data"
+    function getData()
+    {
+        //tampilkan data dari "tb_data"
+        $query = DB::table('tb_data')
+                ->select("id","nama","golongan_darah","usia",
+                "telpon")
+                ->orderBy("id")
+                ->get();
+        //mengirim hasil variabel "query" ke controller "admin"
+        return $query;
+    }
+
 }
